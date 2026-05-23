4a2a8fbed957f006553a8966ca2556732d648df6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-05-23 23:39:52 +0900

    Update Lonely-Integer.cpp

--- Lonely-Integer.cpp
+++ Lonely-Integer.cpp
@@ -95,7 +95,32 @@ string rtrim(const string &str) {
     return s;
 }
 
-vector<string> split(const string &str) {
+
+std::vector<string> split(const string &str)
+{
+    vector<string> tokens;
+    string_view str_view(str);
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str_view.find(' ', start)) != string_view::npos)
+    {
+        if (end > start)
+        {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
+    }
+
+    if (start < str_view.size())
+    {
+        tokens.emplace_back(str_view.substr(start));
+    }
+
+    return tokens;
+}
+
+vector<string> split2(const string &str) {
     vector<string> tokens;
 
     string::size_type start = 0;
