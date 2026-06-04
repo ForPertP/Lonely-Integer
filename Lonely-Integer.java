6aefc9dded1949124b7cbae124fde5d995c608ff
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-06-05 04:01:18 +0900

    Update Lonely-Integer.java

--- Lonely-Integer.java
+++ Lonely-Integer.java
@@ -9,3 +9,21 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+
+    }
+
+}
